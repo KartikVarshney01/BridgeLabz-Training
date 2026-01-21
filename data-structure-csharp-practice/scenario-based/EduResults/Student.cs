c306b612d7d71a204d19064179f21b11eeae6bef
Kartik Varshney <kartikvarshney30@gmail.com>
2026-01-21 15:19:58 +0530

    [Kartik] Add .Added 1 Scenario question on Edu Results

---
+++ data-structure-csharp-practice/scenario-based/EduResults/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.data_structure_csharp_practice.scenario_based.EduResults
+{
+    // Encapsulated Student Class 
+    internal class Student
+    {
+        private static int NextID = 1;
+        public int StudentId { get; set; }
+        public int StudentMarks { get; set; }
+        public string DistrictName { get; set; }
+
+        public Student()
+        {
+            this.StudentId = NextID++;
+        }
+
+        public override string ToString()
+        {
+            return $"Student ID : {StudentId} || Student Marks : {StudentMarks} || Student District : {DistrictName}";
+        }
+    }
+}
