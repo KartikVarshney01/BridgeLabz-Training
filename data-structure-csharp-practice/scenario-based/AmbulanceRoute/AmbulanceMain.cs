83ba6cea10c6498b8e501e8393bb74787ac7a041
Kartik Varshney <kartikvarshney30@gmail.com>
2026-01-22 22:40:34 +0530

    [Kartik] Add .Added 1 Scenario-based qusetion

---
+++ data-structure-csharp-practice/scenario-based/AmbulanceRoute/AmbulanceMain.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.data_structure_csharp_practice.scenario_based.AmbulanceRoute
+{
+    internal class AmbulanceMain
+    {
+        static void Main(String[] args)
+        {
+            AmbulanceMenu start = new AmbulanceMenu();
+            start.HospitalMenu();
+        }
+    }
+}
---
+++ data-structure-csharp-practice/scenario-based/AmbulanceRoute/AmbulanceUtility.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Xml.Linq;
+
+namespace BridgeLabzTraining.data_structure_csharp_practice.scenario_based.AmbulanceRoute
+{
+    internal class AmbulanceUtility : IAmbulance
+    {
+        private HospitalUnitList hospital;
+
+        public AmbulanceUtility()
+        {
+            hospital = new HospitalUnitList();
+            CreateHospital();
+        }
+
+        private void CreateHospital()
+        {
+            hospital.AddUnit("Emergency");
+            hospital.AddUnit("Radiology");
+            hospital.AddUnit("Surgery");
+            hospital.AddUnit("ICU");
+        }
+
+        public void AddPatient()
+        {
+            Console.Write("Enter Patient Name: ");
+            hospital.AddPatient(Console.ReadLine());
+        }
+
+        public void RemovePatient()
+        {
+            Console.Write("Enter Patient Name: ");
+            hospital.RemovePatient(Console.ReadLine());
+        }
+
+        public void ToggleMaintenance()
+        {
+            Console.Write("Enter Unit Name: ");
+            string unit = Console.ReadLine();
+
+            Console.Write("Enter status (true = Active, false = Maintenance): ");
+            bool status = Convert.ToBoolean(Console.ReadLine());
+
+            hospital.ToggleMaintenance(unit, status);
+        }
+
+        public void DisplayStatus()
+        {
+            hospital.DisplayStatus();
+        }
+    }
+}
