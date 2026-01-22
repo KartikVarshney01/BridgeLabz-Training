24cef8a41a1d85f2887be1be892453f26eabe226
Kartik Varshney <kartikvarshney30@gmail.com>
2026-01-22 22:44:12 +0530

    [Kartik] Add .Added Stream Assignment

--- collections-csharp-practice/gcr-codebase/csharp-streams/ImageToByteArray.cs
+++ collections-csharp-practice/gcr-codebase/csharp-streams/ImageToByteArray.cs
@@ -1,28 +1,28 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-//namespace BridgeLabzTraining.collections_csharp_practice.gcr_codebase.csharp_streams
-//{
-//    internal class ImageToByteArray
-//    {
-//        static void Main(string[] args)
-//        {
-//            try
-//            {
-//                byte[] imageBytes = File.ReadAllBytes("image.jpg");
+namespace BridgeLabzTraining.collections_csharp_practice.gcr_codebase.csharp_streams
+{
+    internal class ImageToByteArray
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                byte[] imageBytes = File.ReadAllBytes("image.jpg");
 
-//                using (MemoryStream ms = new MemoryStream(imageBytes))
-//                {
-//                    File.WriteAllBytes("copy.jpg", ms.ToArray());
-//                }
+                using (MemoryStream ms = new MemoryStream(imageBytes))
+                {
+                    File.WriteAllBytes("copy.jpg", ms.ToArray());
+                }
 
-//                Console.WriteLine("Image copied successfully.");
-//            }
-//            catch (IOException ex)
-//            {
-//                Console.WriteLine(ex.Message);
-//            }
-//        }
-//    }
-//}
+                Console.WriteLine("Image copied successfully.");
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
--- collections-csharp-practice/gcr-codebase/csharp-streams/StoreAndRetrieve.cs
+++ collections-csharp-practice/gcr-codebase/csharp-streams/StoreAndRetrieve.cs
@@ -1,32 +1,32 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-//namespace BridgeLabzTraining.collections_csharp_practice.gcr_codebase.csharp_streams
-//{
-//    internal class StoreAndRetrieve
-//    {
-//        static void Main(string[] args)
-//        {
-//            string file = "student.dat";
+namespace BridgeLabzTraining.collections_csharp_practice.gcr_codebase.csharp_streams
+{
+    internal class StoreAndRetrieve
+    {
+        static void Main(string[] args)
+        {
+            string file = "student.dat";
 
-//            // Write
-//            using (BinaryWriter writer = new BinaryWriter(File.Open(file, FileMode.Create)))
-//            {
-//                writer.Write(101);
-//                writer.Write("Kartik");
-//                writer.Write(8.9);
-//            }
+            // Write
+            using (BinaryWriter writer = new BinaryWriter(File.Open(file, FileMode.Create)))
+            {
+                writer.Write(101);
+                writer.Write("Kartik");
+                writer.Write(8.9);
+            }
 
-//            // Read
-//            using (BinaryReader reader = new BinaryReader(File.Open(file, FileMode.Open)))
-//            {
-//                int roll = reader.ReadInt32();
-//                string name = reader.ReadString();
-//                double gpa = reader.ReadDouble();
+            // Read
+            using (BinaryReader reader = new BinaryReader(File.Open(file, FileMode.Open)))
+            {
+                int roll = reader.ReadInt32();
+                string name = reader.ReadString();
+                double gpa = reader.ReadDouble();
 
-//                Console.WriteLine($"{roll} {name} {gpa}");
-//            }
-//        }
-//    }
-//}
+                Console.WriteLine($"{roll} {name} {gpa}");
+            }
+        }
+    }
+}
