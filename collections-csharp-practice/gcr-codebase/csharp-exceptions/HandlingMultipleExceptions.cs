033efdadda8d6fd7f2e82eef04b0f4beae4100a1
Kartik Varshney <kartikvarshney30@gmail.com>
2026-01-23 21:41:38 +0530

    [Kartik] Add .Added Assignment Questions

---
+++ collections-csharp-practice/gcr-codebase/csharp-exceptions/HandlingMultipleExceptions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace BridgeLabzTraining.collections_csharp_practice.gcr_codebase.csharp_exceptions
+{
+    internal class HandlingMultipleExceptions
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("1. Null Array");
+                Console.WriteLine("2. Invalid Index");
+                Console.WriteLine("3. Valid Index");
+                Console.Write("Choose option: ");
+                int choice = Convert.ToInt32(Console.ReadLine());
+
+                int[] nums = null;
+
+                if (choice == 1)
+                {
+                    Console.WriteLine(nums[0]);
+                }
+                else if (choice == 2)
+                {
+                    nums = new int[] { 10, 20, 30 };
+                    Console.WriteLine(nums[5]);
+                }
+                else if (choice == 3)
+                {
+                    nums = new int[] { 10, 20, 30 };
+                    Console.WriteLine($"Value at index {2}: {nums[2]}");
+                }
+            }
+            catch (NullReferenceException)
+            {
+                Console.WriteLine("Array is not initialized!");
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Invalid index!");
+            }
+        }
+    }
+}
